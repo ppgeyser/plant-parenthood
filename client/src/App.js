73b2653487dfedf1789c82e47814f395d5c65596
Peter Geyser <peterpgeyser@gmail.com>
2019-11-04 21:31:38 -0800

    starter code

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Jumbotron, Button } from 'reactstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { About } from "./components/About"
+import { noMatch } from "./components/noMatch/"
 
 function App() {
   return (
-    <div>
-      <Jumbotron>
-        <h1 className="display-3">Hello, world!</h1>
-        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr className="my-2" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p className="lead">
-          <Button color="primary">Learn More</Button>
-        </p>
-      </Jumbotron>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={About} />
+      </Switch>
+      <Switch>
+        <Route component={noMatch} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/components/noMatch/noMatch.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Jumbotron } from "reactstrap"
+
+export const noMatch = props => (
+    <div>
+      <Jumbotron>
+        <h1 className="display-3">404</h1>
+        <p className="lead">Oh my God Karen you can't just ask someone why they're white!</p>
+      </Jumbotron>
+    </div>
+);
