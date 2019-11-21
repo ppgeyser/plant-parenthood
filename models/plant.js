c4199c0f3641cb0bc7a1e6e59677bf7079e8780e
Peter Geyser <peterpgeyser@gmail.com>
2019-11-20 21:24:33 -0800

    Added mongoDB schema and starter code for controller

---
+++ models/plant.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const plantSchema = new Schema({
+    userID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    plantNickname: {
+        type: String
+    },
+    plantName: {
+        type: String,
+        required: true
+    },
+    plantSci: {
+        type: String
+    },
+    plantCare: {
+        soil: {
+            type: String
+        },
+        sun: {
+            type: String,
+            required: true
+        },
+        waterDesc: {
+            type: String
+        },
+        weeks: {
+            type: Integer
+        },
+        days: {
+            type: Integer,
+            required: true
+        }
+    },
+    nonToxic: {
+        type: Boolean
+    },
+    plantPic: {
+        type: String
+    }
+});
+
+const Plant = mongoose.model("Plant", plantSchema);
+
+module.exports = Plant;
