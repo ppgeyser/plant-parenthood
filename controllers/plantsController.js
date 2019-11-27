b2cffd25909f8aed47df9823caf971cea8e37222
Peter Geyser <peterpgeyser@gmail.com>
2019-11-26 16:52:43 -0800

    changed getAll logic to filter by user ID

--- controllers/plantsController.js
+++ controllers/plantsController.js
@@ -1,10 +1,10 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the plantsController
 module.exports = {
   findAll: function(req, res) {
     db.Plant
-      .find(req.query)
+      .find({userID: req.params.id})
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
