2ccf407f70a23970ce33be7848cf655ac6c52ded
Peter Geyser <peterpgeyser@gmail.com>
2019-11-28 15:02:28 -0800

    Fixed issue with path to local mongo db

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userplants");
 
 // Start the API server
 app.listen(PORT, function() {
