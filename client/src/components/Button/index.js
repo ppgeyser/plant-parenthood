05b443eaad23b301aab6b08c4aa29984562d9feb
Zuzanav <zuzanavolny@gmail.com>
2019-11-28 17:13:32 -0800

    survey page css style fixes and submit button

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = props => {
     return (
-        <button onClick={props.onClick}>{props.children}</button>
+        <button className="rounded" onClick={props.onClick}>{props.children}</button>
     )
 }
 
