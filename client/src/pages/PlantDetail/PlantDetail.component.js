f4a53aff4b2895dbec6a98b11bd52d41341faf12
Devin Stewart <49423028+Bigshmow@users.noreply.github.com>
2019-12-01 17:59:38 -0800

    Update PlantDetail.component.js

--- client/src/pages/PlantDetail/PlantDetail.component.js
+++ client/src/pages/PlantDetail/PlantDetail.component.js
@@ -14,6 +14,7 @@ class PlantDetail extends Component {
     constructor(props){
         super(props);
         this.state = {
+            isVisible: false,
             userPlant: {
                 plantCare: {
                     soil: "",
