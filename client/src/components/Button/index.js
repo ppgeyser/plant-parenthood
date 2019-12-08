1c7a08723573dba49700783abe4bd239f4ad4a73
Peter Geyser <peterpgeyser@gmail.com>
2019-12-08 11:42:06 -0800

    added a prop for inline styles

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = props => {
     return (
-        <button className="rounded" onClick={props.onClick}>{props.children}</button>
+        <button className="rounded" style={props.style} onClick={props.onClick}>{props.children}</button>
     )
 }
 
